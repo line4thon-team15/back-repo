53767d4a92796bd08ba08135ea796e4d743de828
HeewonJo <heewonjo.me@gmail.com>
2024-11-09 05:30:14 +0900

    Refactor: return 값 수정

--- reviews/views.py
+++ reviews/views.py
@@ -3,15 +3,24 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Review, ReviewLike
 from .serializers import ReviewSerializer, ReviewLikeSerializer
+from django.db.models import F
 
 class ReviewsAPIView(APIView):
-    def get(self, request):
-        reviews = Review.objects.all()
+    def get(self, request, service_id=None):
+        if service_id:
+            reviews = Review.objects.filter(service_id=service_id)
+        else:
+            reviews = Review.objects.all()
         serializer = ReviewSerializer(reviews, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
-        serializer = ReviewSerializer(data=request.data)
+    def post(self, request, service_id):
+        serializer = ReviewSerializer(
+            data=request.data,
+            context={
+                'request': request,
+                'service_id': service_id
+                })
         if serializer.is_valid():
             serializer.save(writer=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -25,7 +34,7 @@ class ReviewAPIView(APIView):
 
     def put(self, request, pk):
         review = Review.objects.get(id=pk)
-        serializer = ReviewSerializer(review, data=request.data)
+        serializer = ReviewSerializer(review, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
